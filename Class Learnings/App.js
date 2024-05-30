b3a43afe787d5643461a589c2a2c27d427cd1c68
ATRIKA SHOW <readeratrika08@gmail.com>
2024-05-31 01:12:58 +0530

    Nested element in React

--- Class Learnings/App.js
+++ Class Learnings/App.js
@@ -1,9 +1,24 @@
+/* code in html
+ <div id="parent">
+            <div class="child">
+                <h1>Hi, there I'm h1 tag</h1>
+            </div>
+    </div>
+*/
+
+/* code in React */
+
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        React.createElement("h1", {}, "Hi, there I'm h1 tag")
+    )
+)
 const heading = React.createElement(
     "h1",
     { id: "title", xyz: "abc" },
     "Hello React"); // {} => is the place where you will give attribute to the tag
 
-console.log(heading) ; // object
+console.log(heading); // object
 const root = ReactDOM.createRoot(document.getElementById("main"));
 
-root.render(heading);  // render method is converting this object to h1 tag & put it in root
+root.render(parent);  // render method is converting this object to h1 tag & put it in root

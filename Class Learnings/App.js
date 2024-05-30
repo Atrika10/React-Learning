f8bb962302eceea4ed62d19f55850acd6c58c9c5
ATRIKA SHOW <readeratrika08@gmail.com>
2024-05-31 01:20:07 +0530

    Complex Nested element in React

--- Class Learnings/App.js
+++ Class Learnings/App.js
@@ -2,17 +2,34 @@
  <div id="parent">
             <div class="child">
                 <h1>Hi, there I'm h1 tag</h1>
+                <h2>Hi, there I'm h2 tag</h2>
+            </div>
+            <div class="child2">
+                <h1>Hi, there I'm h1 tag</h1>
+                <h2>Hi, there I'm h2 tag</h2>
             </div>
     </div>
 */
 
 /* code in React */
 
-const parent = React.createElement("div", { id: "parent" },
-    React.createElement("div", { id: "child" },
-        React.createElement("h1", {}, "Hi, there I'm h1 tag")
+const parent = React.createElement("div", { id: "parent" }, 
+    [
+        React.createElement("div", { id: "child" }, 
+     [React.createElement("h1", {}, "Hi, there I'm h1 tag"),
+     React.createElement("h2", {}, "Hi, there I'm h2 tag")
+     ]
+    ),
+    React.createElement("div", { id: "child2" }, 
+     [React.createElement("h1", {}, "Hi, there I'm h1 tag"),
+     React.createElement("h2", {}, "Hi, there I'm h2 tag")
+     ]
     )
+    ]
 )
+
+/* if we want to create sibling children inside a div, then in 3rd params of createElement we have to pass element as an array*/ 
+
 const heading = React.createElement(
     "h1",
     { id: "title", xyz: "abc" },

c4f25c905ce7a4e153b4477a11fff3aa77ca18c6
ATRIKA SHOW <readeratrika08@gmail.com>
2024-06-19 17:08:17 +0530

    Sign-in/Sign-up with firebase

--- Projects/NetFlix/src/Utils/Validate.js
+++ Projects/NetFlix/src/Utils/Validate.js
@@ -1,12 +1,12 @@
-export const validateData = (email, password, name) =>{
+export const validateData = (email, password) =>{
 
     const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
     const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
-    const isNameValid = /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(name);
+    // const isNameValid = /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(name);
 
-    if(!isNameValid){
-        return "Name is not valid";
-    }
+    // if(!isNameValid){
+    //     return "Name is not valid";
+    // }
     if(!isEmailValid) {
         return "Email ID is not valid";
     }

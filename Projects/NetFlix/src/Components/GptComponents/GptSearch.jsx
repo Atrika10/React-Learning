914fbf429e05d3f1822c37cfcc066de9913f3f5d
ATRIKA SHOW <readeratrika08@gmail.com>
2024-07-16 23:59:39 +0530

    responsive

--- Projects/NetFlix/src/Components/GptComponents/GptSearch.jsx
+++ Projects/NetFlix/src/Components/GptComponents/GptSearch.jsx
@@ -7,7 +7,9 @@ export default function GptSearch() {
   return (
     <div>
       <div className="-z-10 relative w-full h-full">
-        <img src={MAIN_BACKGROUND} alt="backgroundImg" />
+        <img 
+        className=" h-screen object-cover md:h-screen object-center w-full"
+        src={MAIN_BACKGROUND} alt="backgroundImg" />
         <div className=" absolute inset-0 bg-black opacity-50"></div>    
       </div>
       <div className="absolute top-[30%] w-[100%]">
